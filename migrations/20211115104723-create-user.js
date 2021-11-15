7db86bcfbd3cb13fe4dddd9ee6e7543dadd370fe
mingmoth <mingmoth@gmail.com>
2021-11-15 18:49:20 +0800

    add User sequelize model

--- migrations/20211115104723-create-user.js
+++ migrations/20211115104723-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Todos', {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,9 +12,13 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      isDone: {
-        defaultValue: false,
-        type: Sequelize.BOOLEAN
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -27,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Todos');
+    await queryInterface.dropTable('Users');
   }
 };
