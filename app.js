5a6eda6a6207a86d645b82340559eef652318659
mingmoth <mingmoth@gmail.com>
2021-11-15 17:33:52 +0800

    downgrade express-handlebars from 6 to 5 & add main.hbs

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser') //不用額外載入了
 const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
 
 const app = express()
 const PORT = 3000
 
-// app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-// app.set('view engine', 'hbs')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
